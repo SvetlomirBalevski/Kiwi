0a26d87ae13bcd34d59390c1bbd2c2e4aca3b952
s.balevski@gmail.com <s.balevski@gmail.com>
2019-11-30 15:45:37 +0200

    Add permission to testplans ReorderCasesView

--- tcms/testplans/tests/test_permissions.py
+++ tcms/testplans/tests/test_permissions.py
@@ -6,30 +6,68 @@ import json
 from django.conf import settings
 from django.urls import reverse
 
+
+from tcms.tests import (PermissionsTestCase, factories)
+from tcms.tests.factories import (TestCaseFactory)
 from tcms.testcases.models import TestCasePlan
-from tcms.tests import PermissionsTestCase, factories
 
 
-class ReorderCasesViewTestCase(PermissionsTestCase):
-    """Test case for sorting cases"""
-    permission_label = 'testplans.change_testrun'
+class DeleteCasesViewTestCase(PermissionsTestCase):
+    """Test case for deleting cases from a plan"""
+
+    permission_label = 'testplans.change_testplan'
     http_method_names = ['post']
 
     @classmethod
     def setUpTestData(cls):
+        cls.plan = factories.TestPlanFactory()
+        cls.url = reverse('plan-delete-cases', args=[cls.plan.pk])
 
-        cls.post_data = {'case': [cls.case_3.pk, cls.case_1.pk]}
+        cls.case_1 = TestCaseFactory(
+            plan=[cls.plan])
+        cls.case_2 = TestCaseFactory(
+            plan=[cls.plan])
+        cls.case_3 = TestCaseFactory(
+            plan=[cls.plan])
+        cls.post_data['case'] = [cls.case_1.pk, cls.case_3.pk]
         super().setUpTestData()
 
-        cls.cases_url = reverse('plan-reorder-cases', args=[cls.plan.pk])
+    def verify_post_with_permission(self):
+        self.assertTrue(self.plan.case.filter(
+            pk__in=[self.case_1.pk, self.case_2.pk, self.case_3.pk]).exists())
 
-    def test_order_cases(self):
-        self.client.login(  # nosec:B106:hardcoded_password_funcarg
-            username=self.plan_tester.username,
-            password='password')
+        response = self.client.post(self.url, self.post_data)
+        data = json.loads(str(response.content, encoding=settings.DEFAULT_CHARSET))
 
-        post_data = {'case': [self.case_3.pk, self.case_1.pk]}
-        response = self.client.post(self.cases_url, post_data)
+        self.assertEqual(0, data['rc'])
+        self.assertEqual('ok', data['response'])
+        self.assertFalse(self.plan.case.filter(
+            pk__in=[self.case_1.pk, self.case_3.pk]).exists())
+        self.assertTrue(self.plan.case.filter(
+            pk__in=[self.case_2.pk]).exists())
+
+
+class ReorderCasesViewTestCase(PermissionsTestCase):
+    """Test case for sorting cases"""
+    permission_label = 'testplans.change_testplan'
+    http_method_names = ['post']
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.plan = factories.TestPlanFactory()
+        cls.case_1 = TestCaseFactory(
+            plan=[cls.plan])
+        cls.case_3 = TestCaseFactory(
+            plan=[cls.plan])
+
+        cls.post_data = {'case': [cls.case_3.pk, cls.case_1.pk]}
+        cls.url = reverse('plan-reorder-cases', args=[cls.plan.pk])
+
+        super().setUpTestData()
+
+    def verify_post_with_permission(self):
+
+        response = self.client.post(self.url, self.post_data)
         data = json.loads(str(response.content, encoding=settings.DEFAULT_CHARSET))
 
         self.assertEqual({'rc': 0, 'response': 'ok'}, data)
